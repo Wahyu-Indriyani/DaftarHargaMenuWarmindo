be0b49e96072ec9529c9be5fbaccf9eed4953b52
wahyu <indriyaniwahyu05@gmail.com>
2024-06-20 04:51:27 +0700

    update

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php 
+    include 'header.php'; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Home </title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <main>
+        <div class="home-picture">
+            <img src="img/home.png" width="100%">
+        </div>
+
+        <div class="menu-container">
+            <h1> Menu Best Seller </h1>
+            <div class="menu-items">
+                <div class="menu-item">
+                    <img src="img/Ikan Nila Bakar.jpeg" alt="Ikan Nila Bakar">
+                    <h2> Ikan Nila Bakar </h2>
+                    <p> Aneka Gorengan </p>
+                </div>
+                <div class="menu-item">
+                    <img src="img/Ayam Penyet .jpeg" alt="Ayam Penyet">
+                    <h2> Ayam Penyet </h2>
+                    <p> Aneka Gorengan </p>
+                </div>
+                <div class="menu-item">
+                    <img src="img/Mendoan .jpeg" alt="Tempe Mendoan">
+                    <h2> Tempe Mendoan </h2>
+                    <p> Aneka Gorengan </p>
+                </div>
+                <div class="menu-item">
+                    <img src="img/Risol Mayo.jpeg" alt="Risol Mayo">
+                    <h2> Risol Mayo </h2>
+                    <p> Aneka Gorengan </p>
+                </div>
+                <div class="menu-item">
+                    <img src="img/Telur Geprek .jpeg" alt="Nasi Telur Geprek">
+                    <h2> Nasi Telur Geprek </h2>
+                    <p> Aneka Gorengan </p>
+                </div>
+                <div class="menu-item">
+                    <img src="img/Magelangan .jpeg" alt="Magelangan">
+                    <h2> Magelangan </h2>
+                    <p> Aneka Gorengan </p>
+                </div>
+            </div>
+        </div>       
+    </main>  
+</body>
+</html>
+
+<?php 
+    include 'footer.php'; 
+?>
