77136c10c98d1d0e78a7d93fa28ba2de9bb81f96
wahyu <indriyaniwahyu05@gmail.com>
2024-06-20 04:42:31 +0700

    update footer & header

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title> Home </title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="img/logo.png">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.php"> Home </a></li>
+                <li><a href="menu.html"> Menu </a></li>
+                <li><a href="http://wa.me/+6282326193904"> Contacts </a></li>
+                <li><a href="https://maps.app.goo.gl/n6ezXGY3Ce9C9AES9"> Location </a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
